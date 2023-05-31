eccd49379783fea25abf9bd4b9b8d0f2ad94dede
Mahmudul <mahmudulcsejnu@gmail.com>
2023-05-31 15:23:27 +0300

    Category update done

--- Bookstore/Controllers/CategoryController.cs
+++ Bookstore/Controllers/CategoryController.cs
@@ -22,5 +22,56 @@ namespace BookStore.Controllers
         {
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Custom-Error", "Name & Display order can't be same");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Custom-Error", "Name & Display order can't be same");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
     }
 }
--- Bookstore/Models/Category.cs
+++ Bookstore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
@@ -8,7 +9,9 @@ namespace BookStore.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Range must be between 1 to 100")]
+        public int DisplayOrder { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
 }
