0299d55a16e17e5e4519603af5ea2d8ebbda47ef
Mahmudul <mahmudulcsejnu@gmail.com>
2023-06-06 09:43:12 +0300

    Layout Modified

--- BookStore_Razor/Pages/Categories/Create.cshtml.cs
+++ BookStore_Razor/Pages/Categories/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using BookStore_Razor.DbContexts;
 using BookStore_Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,9 +7,24 @@ namespace BookStore_Razor.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
         public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Categories.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+
+
         }
     }
 }
